2d32204745a6e5fc423799ac7ff47ccbb4d05ebc
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-08-02 14:16:27 +0200

    Add morgan for login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
 
 // Load config
@@ -8,6 +9,11 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  // This will show in the console if there is a request at all
+  app.use(morgan('dev'))
+}
 
 const PORT= process.env.PORT || 3000
 
